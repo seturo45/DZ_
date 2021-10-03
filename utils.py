07945535b4fb742472dc146de22e89420f456423
seturo45 <nikita.kutyaev@gmail.com>
2021-10-03 12:00:05 +0300

    Lesson-4

--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-# Задание 2 и 3
 import requests
 from decimal import *
 from datetime import datetime
@@ -15,19 +14,3 @@ def currency_rates(val):
     day_raw = response[response.find('Date="') + 6:response.find('"', response.find('Date="') + 6)].split('.')
     day, month, year = map(int, day_raw)
     return f"{Decimal(rub.replace(',', '.'))}, {datetime(day=day, month=month, year=year)}"
-
-
-print(currency_rates('usd'))
-print(currency_rates('eur'))
-print(currency_rates('gbp'))
-
-# Задание 4
-import utils
-
-print(utils.currency_rates('usd'))
-
-# # Задание 5
-# import utils
-# import sys
-#
-# print(utils.currency_rates(sys.argv[1]))
